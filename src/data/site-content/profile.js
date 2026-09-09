e2ec6af0f67cc446c3c054762f09a5ab3b42751b
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2026-09-09 22:33:15 +0930

    DEV: update profile pic

--- src/data/site-content/profile.js
+++ src/data/site-content/profile.js
@@ -1,5 +1,5 @@
 export const profile = {
   "name": "Vijini Mallawaarachchi",
   "role": "Research Fellow in Bioinformatics",
-  image: `${import.meta.env.BASE_URL}headshot.png`,
+  "image": `${import.meta.env.BASE_URL}/headshot.png`,
 }
